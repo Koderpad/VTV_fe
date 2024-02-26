ff6465de76bca2758ed8475c59e337c6b2857609
ToDuyVuong <comc5288@gmail.com>
2024-02-26 23:37:06 +0700

    FCM

---
+++ extra/firebaseConfig.tsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAkKXsI4zMyROmgTxc19QMxyDPTO9FY_Mc",
+    authDomain: "notification-vtv.firebaseapp.com",
+    projectId: "notification-vtv",
+    storageBucket: "notification-vtv.appspot.com",
+    messagingSenderId: "306304042714",
+    appId: "1:306304042714:web:0b55683da18c77f19487e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
